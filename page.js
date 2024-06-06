4c3e20c945923e4718d6f7ebc69353ed33fc936f
Jonas A Sihlén <jonasasihlen@gmail.com>
2024-06-07 00:04:58 +0200

    initial commit

---
+++ page.js
@@ -0,0 +1,24 @@
+import { Suspense } from "react";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Problem from "@/components/Problem";
+import FeaturesAccordion from "@/components/FeaturesAccordion";
+import CTA from "@/components/CTA";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Suspense>
+        <Header />
+      </Suspense>
+      <main>
+        <Hero />
+        <Problem />
+        <FeaturesAccordion />
+        <CTA />
+      </main>
+      <Footer />
+    </>
+  );
+}
